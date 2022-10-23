ba3c972a71f92fa448f3b8b125d385d1cd520187
Kosztur Stefan <skosztur@gmail.com>
2022-10-23 13:07:17 +0200

    modified template file

--- main.tf
+++ main.tf
@@ -9,10 +9,7 @@ resource "aws_instance" "ubuntu" {
   ami           = data.aws_ami.ubuntu-ami.id
   instance_type = "t2.micro"
   user_data = file("/assets/user_data.tpl")
-  tags = {
-    Name = "Ubuntu"
-    scope = var.scope
-  }
+  tags = local.common_tags
 
 }
    
